e624b73c5d20812bf2a6a03a9b8a1a86bb05d140
paulstgermain <paulg.stgermain@gmail.com>
2021-09-08 14:23:18 -0400

    adds switch with initial route to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router, Route, Link, Switch,
+} from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
   <Router>
-    <Route>
-      <App />
-    </Route>
+    <Switch>
+      <Route exact path="/">
+        <App />
+      </Route>
+    </Switch>
   </Router>,
   document.getElementById("root"),
 );
