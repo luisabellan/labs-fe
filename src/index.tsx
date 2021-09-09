a6324aad69eab1c9a3504a6ab5958285959bf734
paulstgermain <paulg.stgermain@gmail.com>
2021-09-09 11:52:23 -0400

    removes unneccesary imports from react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import {
-  BrowserRouter as Router, Route, Link, Switch,
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
