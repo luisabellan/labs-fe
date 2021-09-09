293bdd67753c4aad58444bdeeab5c2948afdb8d0
paulstgermain <paulg.stgermain@gmail.com>
2021-09-09 02:51:59 -0400

    completes routing and component stubs

--- src/index.tsx
+++ src/index.tsx
@@ -5,28 +5,10 @@ import {
   BrowserRouter as Router, Route, Link, Switch,
 } from "react-router-dom";
 import App from "./App";
-import Attendance from "./components/Attendance/Attendance";
-import TeamBuilder from "./components/TeamBuilder/TeamBuilder";
-import LearnerSuccess from "./components/LearnerSuccess/LearnerSuccess";
-import FourthComponent from "./components/FourthComponent/FourthComponent";
 
 ReactDOM.render(
   <Router>
     <App />
-    <Switch>
-      <Route path="/attendance">
-        <Attendance />
-      </Route>
-      <Route path="/teambuilder">
-        <TeamBuilder />
-      </Route>
-      <Route path="/learnersuccess">
-        <LearnerSuccess />
-      </Route>
-      <Route path="/fourth">
-        <FourthComponent />
-      </Route>
-    </Switch>
   </Router>,
   document.getElementById("root"),
 );
